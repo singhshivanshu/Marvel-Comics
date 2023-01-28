7b29f8cf2ece42d9e064353cba927e8da511f890
singhshivanshu <shivanshusingh12@gmail.com>
2023-01-29 00:42:35 +0530

    update: pagination

---
+++ src/components/pagination/index.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { usePagination, DOTS } from "../../hooks/usePagination";
+import styles from "./pagination.module.scss";
+
+const Pagination = (props) => {
+  const {
+    onPageChange,
+    totalCount,
+    siblingCount = 2,
+    currentPage,
+    pageSize,
+    className,
+  } = props;
+
+  const paginationRange = usePagination({
+    currentPage,
+    totalCount,
+    siblingCount,
+    pageSize,
+  });
+  console.log(paginationRange);
+  console.log(totalCount, currentPage);
+  if (currentPage === 0 || (paginationRange && paginationRange.length < 2)) {
+    return null;
+  }
+
+  const onNext = () => {
+    onPageChange(currentPage + 1);
+  };
+
+  const onPrevious = () => {
+    onPageChange(currentPage - 1);
+  };
+
+  let lastPage = paginationRange && paginationRange[paginationRange.length - 1];
+  return (
+    <ul className={`${styles.paginationContainer} ${className}`}>
+      <li
+        className={`${styles.paginationItem} ${
+          currentPage === 1 ? styles.disabled : ""
+        }`}
+        onClick={onPrevious}
+      >
+        <div className={`${styles.arrow} ${styles.left}`} />
+      </li>
+      {paginationRange &&
+        paginationRange.map((pageNumber, i) => {
+          if (pageNumber === DOTS) {
+            return (
+              <li
+                key={pageNumber + i}
+                className={`${styles.paginationItem} ${styles.dots}`}
+              >
+                &#8230;
+              </li>
+            );
+          }
+
+          return (
+            <li
+              key={pageNumber + i}
+              className={`${styles.paginationItem} ${
+                pageNumber === currentPage ? styles.selected : ""
+              }`}
+              onClick={() => onPageChange(pageNumber)}
+            >
+              {pageNumber}
+            </li>
+          );
+        })}
+      <li
+        className={`${styles.paginationItem} ${
+          currentPage === lastPage ? styles.disabled : ""
+        }`}
+        onClick={onNext}
+      >
+        <div className={`${styles.arrow} ${styles.right}`} />
+      </li>
+    </ul>
+  );
+};
+
+export default Pagination;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import useKeyToMD5 from "../../hooks/useKeyToMD5";
 import Header from "../../components/header";
-import styles from "./homePage.module.css";
+import styles from "./homePage.module.scss";
 import ComicCard from "../../components/comicCard";
+import Pagination from "../../components/pagination";
+import { QueryClient } from "react-query";
 
 const Home = () => {
   const [timeStamp, hash, publicKey] = useKeyToMD5();
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageLimit = 20;
+  const queryClient = new QueryClient();
 
-  const { isLoading, data, error } = useQuery({
-    queryKey: ["comics"],
+  const { isLoading, data, error, isPreviousData } = useQuery({
+    queryKey: ["comics", currentPage],
     queryFn: () =>
       axios
         .get(
-          `https://gateway.marvel.com:443/v1/public/comics?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
+          `https://gateway.marvel.com:443/v1/public/comics?limit=${pageLimit}&offset=${
+            (currentPage - 1) * 20
+          }&ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
         )
         .then((res) => res.data)
         .catch((error) => console.log(error)),
   });
+
+  useEffect(() => {
+    if (!isPreviousData) {
+      queryClient.prefetchQuery({
+        queryKey: ["comics", currentPage],
+        queryFn: () =>
+          axios
+            .get(
+              `https://gateway.marvel.com:443/v1/public/comics?limit=${pageLimit}&offset=${
+                (currentPage - 1) * 20
+              }&ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
+            )
+            .then((res) => res.data)
+            .catch((error) => console.log(error)),
+      });
+    }
+  }, [data, isPreviousData, currentPage, queryClient]);
+
   console.log(data);
   console.log(isLoading);
   return (
-    <div style={{background: "rgb(71 70 70)"}}>
+    <div style={{ background: "rgb(71 70 70)" }}>
       <Header />
       <div className={styles.wrapper}>
         <div className={styles.container}>
@@ -39,6 +64,15 @@ const Home = () => {
                 );
               })}
           </div>
+          {!isLoading && (
+            <Pagination
+              currentPage={currentPage}
+              totalCount={data && data.data && data.data.total}
+              pageSize={pageLimit}
+              onPageChange={(page) => setCurrentPage(page)}
+              className={styles.paginationBar}
+            />
+          )}
         </div>
       </div>
     </div>
